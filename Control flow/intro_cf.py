b59e3481b7ce404077f2236ef9de1ef6c21a11c4
Your Name <you@example.com>
2022-05-20 17:34:43 +0100

    Intro to control flow content

---
+++ Control flow/intro_cf.py
@@ -0,0 +1,29 @@
+# age = int(input("How old are you?\n"))
+
+
+# if age >= 15:
+#     print("You are old enough to watch this film")
+#     ticket = str(input("Do you have a ticket?\n"))
+#     if ticket == "yes" or ticket == "Yes":
+#         print("Enjoy the film")
+#     elif ticket == "no" or ticket == "No":
+#         print("You still need to buy a ticket")
+#     else:
+#         print("Invalid answer")
+# else:
+#     print("Sorry, try again when you're older")
+
+# rating = str(input("What is the rating"))
+#
+# if rating == "U":
+#     print("Everyone can view ")
+# elif rating == "PG":
+#     print("Needs parental guidance, please check with an adult")
+# elif rating == "12":
+#     print ("You must be at least 12 to watch this")
+# elif rating == "15":
+#     print("You must be at least 15 to watch this")
+# elif rating == "18":
+#     print("You must be at least 18 to watch this")
+
+
