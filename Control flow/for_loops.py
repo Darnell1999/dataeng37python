f0fcf0c608a3dd96fbf95c08c775fa0a6d8bb0cb
Your Name <you@example.com>
2022-05-23 18:20:08 +0100

    Updated version of fizzbuzz, forces integer values

--- Control flow/for_loops.py
+++ Control flow/for_loops.py
@@ -20,14 +20,52 @@
 #     print(team)
 
 
+# current_number = 1
+# max_number = int(input("What number would you like to play up to"))
+# while current_number <= max_number:
+#     if current_number % 3 != 0 and current_number % 5 != 0:
+#         print(current_number)
+#     elif current_number % 15 == 0:
+#         print("FizzBuzz")
+#     elif current_number % 3 == 0:
+#         print("Fizz")
+#     elif current_number % 5 == 0:
+#         print("Buzz")
+#     current_number += 1
+
+invalid = True
+while invalid:
+    games = input("How many games do you want to play?")
+    if games.isdigit():
+        games = int(games)
+        print("Cool let's play")
+        invalid1 = True
+        while invalid1:
+            max_number = input("What number would you like to play up to")
+            if max_number.isdigit():
+                max_number = int(max_number)
+                print("Cool let's play")
+                invalid1 = invalid = False
+            else:
+                print("Please enter a valid answer")
+    else:
+        print("Please enter a valid answer")
+game_number = 0
 current_number = 1
-while current_number <100:
-    if current_number % 3 != 0 and current_number % 5 != 0:
-        print(current_number)
-    elif current_number % 15 == 0:
-        print("FizzBuzz")
-    elif current_number % 3 == 0:
-        print("Fizz")
-    elif current_number % 5 == 0:
-        print("Buzz")
-    current_number += 1
+while game_number < games:
+    print(f"This is game number {game_number+1}")
+    while current_number <= max_number:
+        if current_number % 3 != 0 and current_number % 5 != 0:
+            print(current_number)
+        elif current_number % 15 == 0:
+            print("FizzBuzz")
+        elif current_number % 3 == 0:
+            print("Fizz")
+        elif current_number % 5 == 0:
+            print("Buzz")
+        current_number += 1
+    game_number += 1
+    current_number = 0
+print("Thanks for playing with me")
+
+
