1869b78900ad5f67079e204b4f6c6e902e51fa63
Your Name <you@example.com>
2022-06-09 15:18:35 +0100

    Python lists practice

--- variables_intro/collections_info/lists_intro.py
+++ variables_intro/collections_info/lists_intro.py
@@ -1,22 +1,48 @@
-shopping_list = ["bread", "bananas", "biscuits", "oat milk"]
-
-shopping_list.append("cereal")
-print(shopping_list)
-shopping_list.append("chocolate")
-
-print(len(shopping_list))
-
-shopping_list.remove("biscuits")
-print(shopping_list)
-
-print(shopping_list.pop())
-print(shopping_list)
-
-nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(nested_list)
-print(nested_list[1])
-print(nested_list[1][-1])  # Should not be nested
+# shopping_list = ["bread", "bananas", "biscuits", "oat milk"]
+#
+# shopping_list.append("cereal")
+# print(shopping_list)
+# shopping_list.append("chocolate")
+#
+# print(len(shopping_list))
+#
+# shopping_list.remove("biscuits")
+# print(shopping_list)
+#
+# print(shopping_list.pop())
+# print(shopping_list)
+#
+# nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print(nested_list)
+# print(nested_list[1])
+# print(nested_list[1][-1])  # Should not be nested
 
 # Tuples are IMMUTABLE
 # Similar to lists otherwise
 # DICTIONARY NAME [ KEY ]
+
+# team_home = {
+# 'Arsenal': 'The Carpet',
+# 'Man City': 'The Emptyhad',
+# 'Tottenham': 'Sh*te Hart Lane',
+# 'Man Utd': 'Theatre of Nightmares'
+# }
+#
+# print(team_home['Tottenham'])
+#
+# print(team_home.keys())
+# print(team_home.values())
+# print(team_home.items())
+
+mcu = {"Iron Man", "Thor", "Captain America", "Hulk"}  # This is a set, sets are unordered
+mcu.add("Hawkeye")
+mcu.add("Thor")
+mcu.discard("Iron Man")
+print(mcu)
+
+
+
+
+
+
+
